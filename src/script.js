fbebda52fc949ab55763f6e81a06876c062e7d8c
Kristjan Gannon <gannonk08@gmail.com>
2016-07-18 15:20:23 -0600

    Checkerboard  Commit

--- src/script.js
+++ src/script.js
@@ -2,29 +2,6 @@
 
 //Checkerboard
 
-// for (var j = 0; j < 9; j++) {
-//   for (var i = 0; i < 9; i++) {
-//     if(i%2===j%2){
-//       var div = document.createElement('div')
-//       document.body.appendChild(div)
-//       div.style.width = '11.1%';
-//       div.style.paddingBottom = '11.1%';
-//       div.style.backgroundColor = "black";
-//       div.style.float = 'left';
-//     }
-//     else{
-//       var div = document.createElement('div')
-//       document.body.appendChild(div)
-//       div.style.width = '11.1%';
-//       div.style.paddingBottom = '11.1%';
-//       div.style.backgroundColor = "red";
-//       div.style.float = 'left';
-//     }
-//   };
-// }
-
-Random Colors
-
 for (var j = 0; j < 9; j++) {
   for (var i = 0; i < 9; i++) {
     if(i%2===j%2){
@@ -32,7 +9,7 @@ for (var j = 0; j < 9; j++) {
       document.body.appendChild(div)
       div.style.width = '11.1%';
       div.style.paddingBottom = '11.1%';
-      div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+      div.style.backgroundColor = "black";
       div.style.float = 'left';
     }
     else{
@@ -40,11 +17,34 @@ for (var j = 0; j < 9; j++) {
       document.body.appendChild(div)
       div.style.width = '11.1%';
       div.style.paddingBottom = '11.1%';
-      div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+      div.style.backgroundColor = "red";
       div.style.float = 'left';
     }
   };
 }
+//
+// Random Colors
+//
+// for (var j = 0; j < 9; j++) {
+//   for (var i = 0; i < 9; i++) {
+//     if(i%2===j%2){
+//       var div = document.createElement('div')
+//       document.body.appendChild(div)
+//       div.style.width = '11.1%';
+//       div.style.paddingBottom = '11.1%';
+//       div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+//       div.style.float = 'left';
+//     }
+//     else{
+//       var div = document.createElement('div')
+//       document.body.appendChild(div)
+//       div.style.width = '11.1%';
+//       div.style.paddingBottom = '11.1%';
+//       div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+//       div.style.float = 'left';
+//     }
+//   };
+// }
 // for (var j = 0; j < 9; j++) {
 //   for (var i = 0; i < 9; i++) {
 //     if(i%2===j%2){
