89d0a930b52913130d5eec7d5e75cb4245c0461e
Kristjan Gannon <gannonk08@gmail.com>
2016-07-18 15:18:15 -0600

    Random  Commit

--- src/script.js
+++ src/script.js
@@ -23,28 +23,8 @@
 //   };
 // }
 
-//Random Colors
+Random Colors
 
-// for (var j = 0; j < 9; j++) {
-//   for (var i = 0; i < 9; i++) {
-//     if(i%2===j%2){
-//       var div = document.createElement('div')
-//       document.body.appendChild(div)
-//       div.style.width = '11.1%';
-//       div.style.paddingBottom = '11.1%';
-//       div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-//       div.style.float = 'left';
-//     }
-//     else{
-//       var div = document.createElement('div')
-//       document.body.appendChild(div)
-//       div.style.width = '11.1%';
-//       div.style.paddingBottom = '11.1%';
-//       div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-//       div.style.float = 'left';
-//     }
-//   };
-// }
 for (var j = 0; j < 9; j++) {
   for (var i = 0; i < 9; i++) {
     if(i%2===j%2){
@@ -52,17 +32,37 @@ for (var j = 0; j < 9; j++) {
       document.body.appendChild(div)
       div.style.width = '11.1%';
       div.style.paddingBottom = '11.1%';
-      div.style.background: #466368;
-      div.style.background: radial-gradient(#648880, #293f50);
+      div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
       div.style.float = 'left';
     }
     else{
       var div = document.createElement('div')
       document.body.appendChild(div)
       div.style.width = '11.1%';
-      div.style.background: #466368;
-      div.style.background: radial-gradient(#648880, #293f50);
+      div.style.paddingBottom = '11.1%';
+      div.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
       div.style.float = 'left';
     }
   };
 }
+// for (var j = 0; j < 9; j++) {
+//   for (var i = 0; i < 9; i++) {
+//     if(i%2===j%2){
+//       var div = document.createElement('div')
+//       document.body.appendChild(div)
+//       div.style.width = '11.1%';
+//       div.style.paddingBottom = '11.1%';
+//       div.style.background: #466368;
+//       div.style.background: radial-gradient(#648880, #293f50);
+//       div.style.float = 'left';
+//     }
+//     else{
+//       var div = document.createElement('div')
+//       document.body.appendChild(div)
+//       div.style.width = '11.1%';
+//       div.style.background: #466368;
+//       div.style.background: radial-gradient(#648880, #293f50);
+//       div.style.float = 'left';
+//     }
+//   };
+// }
